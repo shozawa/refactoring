f106b2cd10519523617c49287c5881163831948b
shozawa <t.shozawa@mugenup.com>
2017-10-25 11:24:30 +0900

    add order

---
+++ order.rb
@@ -0,0 +1,13 @@
+class Order
+  def price
+    base_price = @quantity * @item_price
+    level_of_discount = 1
+    level_of_discount = 2 if @quantity > 100
+    discounted_price(base_price, level_of_discount)
+  end
+
+  def discounted_price(base_price, level_of_discount)
+    return base_price * 0.1 if level_of_discount == 2
+    base_price * 0.05
+  end
+end
