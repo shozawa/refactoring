345ec42caa240315cc52e2521d4bd3ee3b08a56a
shozawa <t.shozawa@mugenup.com>
2017-10-25 11:32:28 +0900

    メソッドのインライン化

--- order.rb
+++ order.rb
@@ -1,15 +1,11 @@
 class Order
   def price
-    discounted_price
+    return base_price * 0.1 if discount_level == 2
+    base_price * 0.05
   end
 
   def base_price
     @quantity * @item_price
-  end
-
-  def discounted_price
-    return base_price * 0.1 if discount_level == 2
-    base_price * 0.05
   end
 
   def discount_level
