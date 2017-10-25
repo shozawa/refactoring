6e21492dd02da0310caa6b66c7457b950b3ebccc
shozawa <t.shozawa@mugenup.com>
2017-10-25 11:31:33 +0900

    引数の削除

--- order.rb
+++ order.rb
@@ -1,13 +1,13 @@
 class Order
   def price
-    discounted_price(base_price)
+    discounted_price
   end
 
   def base_price
     @quantity * @item_price
   end
 
-  def discounted_price(base_price)
+  def discounted_price
     return base_price * 0.1 if discount_level == 2
     base_price * 0.05
   end
