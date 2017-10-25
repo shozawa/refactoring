a6e3999906865fee93d5c9c0d992392aea62a03b
shozawa <t.shozawa@mugenup.com>
2017-10-25 11:04:17 +0900

    computeメソッドの追加

--- account.rb
+++ account.rb
@@ -29,5 +29,16 @@ class Account
       @quantity = quantity
       @year_to_date = year_to_date
     end
+
+    def compute
+      @important_value_1 = (input_val * quantity) + account.delta
+      @important_value_2 = (input_val * year_to_date) + 100
+      if (year_to_date - important_value_1) > 100
+        @important_value_2 -= 20
+      end
+      @important_value_3 = important_value_2 * 7
+      # ...
+      important_value_3 - 2 * important_value_1
+    end
   end
 end
