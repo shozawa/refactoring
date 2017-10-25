6deb5dc6e06568ee944812b4217193a101253ce3
shozawa <t.shozawa@mugenup.com>
2017-10-25 11:26:44 +0900

    値引きレベルの計算をメソッドとして抽出

--- order.rb
+++ order.rb
@@ -1,13 +1,16 @@
 class Order
   def price
     base_price = @quantity * @item_price
-    level_of_discount = 1
-    level_of_discount = 2 if @quantity > 100
-    discounted_price(base_price, level_of_discount)
+    discounted_price(base_price, discount_level)
   end
 
   def discounted_price(base_price, level_of_discount)
     return base_price * 0.1 if level_of_discount == 2
     base_price * 0.05
+  end
+
+  def discount_level
+    return 2 if @quantity > 100
+    return 1
   end
 end
